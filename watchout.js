b77930f025305969fdd97a4f4f736a22bdc16871
Luke <lramsey177@gmail.com>
2014-03-07 17:28:37 -0800

    Added background, created functions for enemies and hero and created default board.

--- watchout.js
+++ watchout.js
@@ -1,36 +1,53 @@
 // start slingin' some d3 here.
 var svg = d3.select(".container").append("svg");
-var circles = svg.selectAll("circle");
-
-svg.attr({"width": 500, "height": 500})
-  .style("background-color", "black");
-
-circles
-  .data([10,10,10,10,10,10])
-  .enter()
-  .append("circle")
-  .attr("r", function(d) { return d })
-  .attr("cx", function() { return Math.random() * 400 })
-  .attr("cy", function() { return Math.random() * 400 })
-  .style("fill", function() {
-    return "hsl(" + Math.random() * 360 + ",100%,50%)";
-  })
-  .transition()
-  .attr("cx", function() { return Math.random() * 400 })
-  .attr("cy", function() { return Math.random() * 400 })
-  .style("fill", function() {
-    return "hsl(" + Math.random() * 360 + ",100%,50%)";
-  })
-  .attr("r", function(d) { return Math.random() * 35 + 10 })
-
-var moveCircles = function() {
-  svg.selectAll("circle").transition()
-  .attr("cx", function() { return Math.random() * 400 })
-  .attr("cy", function() { return Math.random() * 400 })
-  .style("fill", function() {
-    return "hsl(" + Math.random() * 360 + ",100%,50%)";
-  })
-  .attr("r", function(d) { return Math.random() * 35 + 10})
-  setTimeout(function() { moveCircles() }, 1000);
+var board = {
+  "height": 600,
+  "width": 800,
+  "paddingX": 400,
+  "paddingY": 200,
+  "enemies": 20
+};
+var makeHero = function(){
+  svg.selectAll("rect")
+    .data([15])
+    .enter()
+    .append('rect')
+    .attr("x", board.paddingX + board.width/2)
+    .attr("y", board.paddingY + board.height/2)
+    .attr("width", function(d) { return d })
+    .attr("height", function(d) { return d })
+    .style("fill", "white")
 }
-moveCircles();
+
+var makeEnemies = function (num) {
+  var enemies = [];
+  for (var i = 0; i < num; i++) {
+    enemies.push(i);
+  }
+  svg.selectAll("circle")
+    .data(enemies)
+    .enter()
+    .append("circle")
+    .attr("r", function(d) { return Math.random() * 100/num + 10 })
+    .attr("cx", function() { return Math.random() * board.width + 400 })
+    .attr("cy", function() { return Math.random() * board.height + 200})
+    .style("fill", function() {
+      return "hsl(" + Math.random() * 360 + ",100%,50%)";
+    })
+};
+
+var moveEnemies = function() {
+  svg.selectAll("circle").transition()
+  .duration(1700)
+  .attr("cx", function() { return Math.random() * board.width + 400 })
+  .attr("cy", function() { return Math.random() * board.height + 200})
+  .style("fill", function() {
+    return "hsl(" + Math.random() * 360 + ",100%,50%)";
+  })
+  .attr("r", function(d) { return Math.random() * 120/board.enemies + 10})
+  setTimeout(function() { moveEnemies() }, 1500);
+}
+
+makeEnemies(board.enemies);
+moveEnemies();
+makeHero();
